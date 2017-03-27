4e53e0ad4668cc83f83f0b44bd8863345ae82818
Alexander Orlov <79202875363@ya.ru>
2017-03-27 11:04:42 +0300

    Получил размеры экрана

--- HowManyMore/app/src/main/java/com/example/alex/howmanymore/MainActivity.java
+++ HowManyMore/app/src/main/java/com/example/alex/howmanymore/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.alex.howmanymore;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+    private final String LOG_TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getScreenSize();
+    }
+
+    private void getScreenSize(){
+        Log.d(LOG_TAG, "Start getScreenSize");
+        Point size = new Point();
+        WindowManager windowManager = getWindowManager();
+        windowManager.getDefaultDisplay().getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        Log.d(LOG_TAG, "screen width = " + width + ", screen height = " + height);
+        Log.d(LOG_TAG, "End getScreenSize");
     }
 }
