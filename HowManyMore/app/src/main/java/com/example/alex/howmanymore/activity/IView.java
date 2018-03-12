88f0127ff4ddf8e50cc82c2bcf4a99efba222659
Alexander Orlov <79202875363@ya.ru>
2018-03-12 12:08:29 +0300

    Рефакторинг. Перевод приложения на Clean Architecture

--- HowManyMore/app/src/main/java/com/example/alex/howmanymore/activity/IView.java
+++
@@ -1,8 +0,0 @@
-package com.example.alex.howmanymore.activity;
-
-/**
- * Created by alex on 03.10.17.
- */
-
-public interface IView {
-}
--- HowManyMore/app/src/main/java/com/example/alex/howmanymore/fragments/IOnSelectedCountryListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.alex.howmanymore.fragments;
-
-/**
- * Created by alex on 19.11.17.
- */
-
-public interface IOnSelectedCountryListener {
-    void onChooseCountry(int flag);
-}
